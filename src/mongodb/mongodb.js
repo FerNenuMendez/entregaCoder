eae640bf0e125706aab9a140a07242b87151cf07
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2023-12-09 14:48:24 -0300

    modificion de mongodb.js

--- src/mongodb/mongodb.js
+++ src/mongodb/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
-//await mongoose.connect('mongodb+srv://fermodmen:Feer1688NenuCo2307@clusterdata.xlbloof.mongodb.net/')
+//await mongoose.connect('mongodb+srv://fermodmen:<>@clusterdata.xlbloof.mongodb.net/')
 
 
 export { ProductManager } from '../dao/models/mdbProductManager.js'
