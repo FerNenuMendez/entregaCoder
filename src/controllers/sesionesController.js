13b07a299663d2d46ee0f5b18567d216031fc7cd
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-01-12 14:27:25 -0300

    cambios de login

--- src/controllers/sesionesController.js
+++ src/controllers/sesionesController.js
@@ -1,6 +1,5 @@
 import { UsuarioManager } from '../mongodb/mongodb.js'
-import { ADMIN_EMAIL } from '../utils/config.js'
-import { hasheadasSonIguales } from '../utils/criptografia.js'
+
 
 export async function getControllerSesion(req, res) {
     if (req.session['user']) {
@@ -9,17 +8,16 @@ export async function getControllerSesion(req, res) {
     res.status(400).json({ status: 'error', message: 'no hay una sesion iniciada' })
 }
 
-export async function postControllerSesion(req, res) {
-    const { email, password } = req.body
-    try {
-
-        const datosUsuario = await UsuarioManager.login(email, password)
-        req.session['user'] = datosUsuario
-        res.status(201).json({ status: 'success', message: 'login success' })
-    } catch (error) {
-        return res.status(401).json({ status: 'error', message: error.message })
-    }
-}
+// export async function postControllerSesion(req, res) {
+//     const { email, password } = req.body
+//     try {
+//         const datosUsuario = await UsuarioManager.login(email, password)
+//         req.session['user'] = datosUsuario
+//         res.status(201).json({ status: 'success', message: 'login success' })
+//     } catch (error) {
+//         return res.status(401).json({ status: 'error', message: error.message })
+//     }
+// }
 
 export async function deleteControllerSesion(req, res) {
     req.session.destroy(err => {
--- src/routers/api/sesionesRouter.js
+++ src/routers/api/sesionesRouter.js
@@ -1,8 +1,26 @@
 import { Router } from 'express'
-import { postControllerSesion, deleteControllerSesion, getControllerSesion } from '../../controllers/sesionesController.js'
+import { deleteControllerSesion, getControllerSesion } from '../../controllers/sesionesController.js'
+import { UsuarioManager } from '../../mongodb/mongodb.js'
 
 export const sesionesRouter = Router()
 
-sesionesRouter.post('/', postControllerSesion)
+sesionesRouter.post('/',
+    async (req, res, next) => {
+        const { email, password } = req.body
+        try {
+            const datosUsuario = await UsuarioManager.login(email, password)
+            req.session['user'] = datosUsuario
+            next()
+        } catch (error) {
+            next(error)
+        }
+    },
+    async (req, res, next) => {
+        res.status(201).json({ status: 'success', message: 'login success' })
+    },
+    (error, req, res, next) => {
+        res.status(401).json({ status: 'error', message: error.message })
+    }
+)
 sesionesRouter.get('/current', getControllerSesion)
 sesionesRouter.delete('/current', deleteControllerSesion)
